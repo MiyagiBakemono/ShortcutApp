17e7e2f6cb79a672046a37ba5fe4d2f942df8af7
touro <touro@DESKTOP-FTMM3DL>
2024-11-11 07:49:10 +0900

    プロジェクト ファイルを追加します。

---
+++ ShortcutApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ShortcutApp
+{
+    partial class form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ショートカット登録";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(356, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "form1";
+            this.Text = "ショートカットアプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ShortcutApp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ShortcutApp
+{
+    public partial class form1 : Form
+    {
+        //path保存用リスト
+        List<string> pathList = new List<string>();
+
+        // ボタン配置の初期位置
+        int x = 10; // 横座標
+        int y = 10; // 縦座標
+        int width = 40;//ボタン間の幅
+
+        public form1 ()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        //ボタンクリックしてFile-PATH取得
+        private void button1_Click ( object sender, EventArgs e )
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //取得するファイルの種類を指定
+            openFileDialog.Filter = "All Files|*.exe;*.url*";
+
+            //ファイルを選択
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //pathListに追加
+                pathList.Add(openFileDialog.FileName);
+
+                for (int i = 0; i < pathList.Count; i++)
+                {
+                    //ボタンインスタンス生成
+                    Button button = new Button();
+
+                    //ボタンのプロパティ設定
+                    button.Text = pathList.Count.ToString();
+                    button.Location = new Point(10, 10 + width * i);
+
+                    //formにボタンを追加表示
+                    Controls.Add(button);
+                }
+            }
+        }
+    }
+}
