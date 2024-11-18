ec28e25e2f99e3c8204e69ebff40ca7caf06111c
janefixa77@gmail.com <janefixa77@gmail.com>
2024-11-18 21:47:13 +0900

    11/18日、ボタンの削除機能と黒い砂漠のアプリ起動キャンセル時に落ちるバグを修正

--- ShortcutApp/Form1.Designer.cs
+++ ShortcutApp/Form1.Designer.cs
@@ -30,50 +30,65 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(368, 12);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 74);
+            this.button1.Size = new System.Drawing.Size(77, 21);
             this.button1.TabIndex = 0;
-            this.button1.Text = "ショートカット登録";
+            this.button1.Text = "追加";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(356, 135);
+            this.textBox1.Location = new System.Drawing.Point(613, 109);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 19);
             this.textBox1.TabIndex = 5;
             // 
-            // textBox2
+            // button2
             // 
-            this.textBox2.Location = new System.Drawing.Point(356, 197);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 19);
-            this.textBox2.TabIndex = 6;
+            this.button2.Location = new System.Drawing.Point(613, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 22);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DebugButton";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.aToolStripMenuItem.Text = "削除";
+            this.aToolStripMenuItem.Click += new System.EventHandler(this.deleteShortcuts);
             // 
             // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "form1";
             this.Text = "ショートカットアプリ";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,9 +97,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
     }
 }
 
--- ShortcutApp/Form1.cs
+++ ShortcutApp/Form1.cs
@@ -11,53 +11,149 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace ShortcutApp
 {
     public partial class form1 : Form
     {
+        private int [,] locations;
+
+
         //path保存用リスト
         List<string> pathList = new List<string>();
+        List<Button> buttonList = new List<Button>();
 
         // ボタン配置の初期位置
-        int x = 10; // 横座標
-        int y = 10; // 縦座標
-        int width = 40;//ボタン間の幅
+        private int x = 12; // 横座標
+        private int y = 12; // 縦座標
+        private int space = 40;//ボタン間の幅
+        //ボタンのサイズ
+        private int buttonSize = 64;
+        int plusXPos = 1;//ボタンの位置変更用係数
+
+        //pathList要素数カウント用変数
+        int currentListCount;
+
+        //ボタン識別用変数
+        int buttonTag=0;
+
+        //ファイルダイアログ
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        string openFilepath;
 
         public form1 ()
         {
             InitializeComponent();
-            
-
         }
 
         //ボタンクリックしてFile-PATH取得
         private void button1_Click ( object sender, EventArgs e )
         {
-
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            //取得するファイルの種類を指定
+            //取得できるファイルの種類を指定
             openFileDialog.Filter = "All Files|*.exe;*.url*";
 
-            //ファイルを選択
+            //ファイルを選択してOK押したら
             if(openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                //pathListに追加
+                //パスの要素数をcurrentListCountに追加
+                currentListCount = pathList.Count;
+                //選んだファイルのパスをpathListに追加
                 pathList.Add(openFileDialog.FileName);
 
-                for (int i = 0; i < pathList.Count; i++)
-                {
-                    //ボタンインスタンス生成
-                    Button button = new Button();
+                openFilepath = openFileDialog.FileName;
 
-                    //ボタンのプロパティ設定
-                    button.Text = pathList.Count.ToString();
-                    button.Location = new Point(10, 10 + width * i);
-
-                    //formにボタンを追加表示
-                    Controls.Add(button);
-                }
+                //ボタン生成メソッドを実行
+                buttonCreate();
             }
         }
+
+
+        //ボタン生成メソッド
+        private void buttonCreate ()
+        {
+            //pathListがcurrentListCountより大きくなったら(pathListに要素数が増えたら)
+            if(pathList.Count > currentListCount)
+            {
+                //ボタンを生成、リストに追加
+                Button button = new Button();//ボタンを生成
+                buttonList.Add(button); //リストに追加
+
+                // 取得したいファイルのパス
+                string filePath = openFilepath;
+
+                //ボタンのプロパティ設定
+                button.Text = openFilepath; //ボタンのテキストに取得したファイル名代入
+                button.Size = new Size(buttonSize,buttonSize);
+                button.Location = new Point(x, y + space * plusXPos);//Pos移動
+                button.Click += newButton_Click; //クリックイベント指定
+                button.Tag = buttonTag; //ボタン識別用のTag付け
+                button.MouseDown += button2_MouseDown; //右クリックされた時の処理
+                buttonTag++;//Tagに1足す
+                plusXPos += 2;//次生成ボタンX軸移動用変数にx足す
+
+                //formにボタンを追加表示
+                Controls.Add(button);
+            }
+        }
+
+        //生成されたボタン用クリックイベント
+        private void newButton_Click( object sender, EventArgs e )
+        {
+            //path先のファイル起動
+            try
+            {
+                Process.Start(openFileDialog.FileName);
+            }
+            catch { }
+        }
+
+        //ContextMenuStripで呼び出す削除機能
+        private void deleteShortcuts ( object sender, EventArgs e)
+        {
+            //ダイアログ表示
+            DialogResult result = MessageBox.Show(
+                "このショートカットを削除しますか?",
+                "確認", 
+                MessageBoxButtons.YesNo
+            );
+
+            //ダイアログでYES押されたら
+            if (result == DialogResult.Yes)
+            {
+                Controls.Remove(clickedButton);
+                textBox1.Text = "delete";
+
+                //pathList内、buttonの番号番の要素削除
+                //pathList.RemoveAt((int)clickedButton.Tag);
+            }
+        }
+
+        private MouseEventArgs mouseEventArgs;
+        private Button clickedButton;
+
+        //MouseDown時に呼び出す処理
+        private void button2_MouseDown ( object sender, MouseEventArgs e )
+        {
+            //clickedButtonにクリックしたbuttonの情報を代入
+            clickedButton = sender as Button;
+
+            //マウスの情報    
+            mouseEventArgs = e;
+
+            //ボタン上で右クリックしたら
+            if(mouseEventArgs.Button == MouseButtons.Right)
+            {
+                //contextMenuStrip1をカーソルの位置に表示
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void locationControl ()
+        {
+            
+        }
+
     }
 }
